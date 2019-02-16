0bf0420f9c04a34567e118d5d96dda87150871fc
Alberto Barrago <a.barrago69@gmail.com>
2019-02-16 17:18:48 +0100

    added esibition, perforamce, images and text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'progetto', loadChildren: './progetto/progetto.module#ProgettoPageModule' },
+  { path: 'esibizione', loadChildren: './esibizione/esibizione.module#EsibizionePageModule' },
+  { path: 'performance', loadChildren: './performance/performance.module#PerformancePageModule' },
 ];
 @NgModule({
   imports: [
