22451199a7db4781500aa408e7e5b015b5364f08
Alberto Barrago <a.barrago@sinossi.it>
2019-02-21 16:01:28 +0100

    fix

--- src/app/performance/performance.page.ts
+++ src/app/performance/performance.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
+import { Device } from '@ionic-native/device/ngx';
 
 @Component({
   selector: 'app-performance',
@@ -7,14 +8,27 @@ import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
   styleUrls: ['./performance.page.scss'],
 })
 export class PerformancePage implements OnInit {
+    private appSettings: any;
 
-    constructor(private photoViewer: PhotoViewer) {
+    constructor(
+        private photoViewer: PhotoViewer,
+        private device: Device) {
     }
 
   ngOnInit() {
   }
-  zoomImage = (imageDate) => {
-        // this.photoViewer.show(imageDate);
-  }
+
+  zoomImage(src, title) {
+        const options = {
+            share: false,
+            closeButton: this.device.platform === 'iOS',
+            copyToReference: this.device.platform === 'iOS'
+        };
+
+        // let srcUrl = src.replace(/%20/g, " ");
+        const srcUrl = decodeURIComponent(src);
+
+         this.photoViewer.show(srcUrl, title, options);
+    }
 
 }
