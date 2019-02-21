904a6baf6af9f8982421d644aeb751b86a984a78
Alberto Barrago <a.barrago69@gmail.com>
2019-02-21 19:05:26 +0100

    last plugin for zooming dont work with ionic4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+
 export function createTranslateLoader(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,9 +1,8 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicImageViewerModule } from 'ionic-img-viewer';
+
 
 import { TabsPageRoutingModule } from './tabs.router.module';
 
@@ -14,8 +13,7 @@ import { TabsPage } from './tabs.page';
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule,
-    IonicImageViewerModule
+    TabsPageRoutingModule
   ],
   declarations: [TabsPage]
 })
