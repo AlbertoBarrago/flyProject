e5c701dcb4773de0967d52edcb6ad4d2ec6dbef6
Alberto Barrago <a.barrago@sinossi.it>
2019-02-21 14:58:29 +0100

    added maps and many fix and template maps :)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,18 +1,25 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit{
+export class Tab2Page implements OnInit {
 
 
-    constructor(private translate: TranslateService) {};
+    constructor(private translate: TranslateService, private launchNavigator: LaunchNavigator) {}
 
-    ngOnInit() {
+    ngOnInit() {}
 
+    goTo(destination) {
+        this.launchNavigator.navigate(destination)
+        .then(
+            success => console.log('Launched navigator'),
+            error => console.log('Error launching navigator', error)
+        );
     }
 
     selectIta() {
